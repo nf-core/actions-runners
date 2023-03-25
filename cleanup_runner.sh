72dc004265646e2b9bba6d6671a723cb6dd7cab2
Alexander Peltzer <apeltzer@users.noreply.github.com>
2023-03-25 21:07:52 +0100

    Update cleanup_runner.sh
    Clean images

--- cleanup_runner.sh
+++ cleanup_runner.sh
@@ -2,3 +2,4 @@
 #This script runs after each job to automatically remove temporary files in order to keep the runner clean and nice
 find /home/runner/work/ -mindepth 1 -delete
 docker system prune --volumes --force
+docker image prune -a
