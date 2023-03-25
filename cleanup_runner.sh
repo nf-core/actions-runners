409a7358e39fa54191809aabf15fee49ec819a0b
Alexander Peltzer <apeltzer@users.noreply.github.com>
2023-03-25 20:36:35 +0100

    Update cleanup_runner.sh

--- cleanup_runner.sh
+++ cleanup_runner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #This script runs after each job to automatically remove temporary files in order to keep the runner clean and nice
-find /home/ubuntu/actions-runner/_work/ -mindepth 1 -delete
+find /home/runner/work/ -mindepth 1 -delete
 docker system prune --volumes --force
